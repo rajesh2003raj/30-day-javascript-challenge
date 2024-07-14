c9462acdbd09c3a6e7c25cf3c76f3768ca42a0d4
rajesh kumar <rajeshvk2003@gmail.com>
2024-07-14 21:48:41 +0530

    day 1 challenge

---
+++ 1 day/index.js
@@ -0,0 +1,57 @@
+/// task one 
+var number=10;
+console.log(number);
+
+/// task 2
+let name1="rajesh ";
+console.log(name1);
+
+/// task 3 
+ const val=false;
+ console.log(val);
+
+ // task 4 
+
+ let number1=34;
+ console.log(typeof(number1));
+ let  email='rajeshvk';
+ console.log(typeof(email));
+
+  console.log(typeof(val));
+ 
+  let object={
+     fullName:"rajesh",
+      rollNo: "0111IT211087"
+  }  
+
+ console.log(typeof(object));
+
+/// // /
+ const arr=[1,2,3,4,'first',6]
+ 
+ console.log(typeof(arr));
+
+
+ /// task 5 
+
+ let number3=134;
+ console.log(number3);
+ number3=321;
+ console.log(number3);
+
+ /// task 6
+  const num=32;
+   console.log(num);
+   //// error 
+//  num=43;
+  // console.log(num);
+
+
+
+
+   ///// features 
+   /// javascipt is consider dynamic language  because it datatype changes  dynamically 
+   /// when we declare an array and check it datatpes it show an object it is right because in javascript array is a part of object
+
+   /// when we declared a variable constant then we have reassign it value again it gives an error
+   ////
